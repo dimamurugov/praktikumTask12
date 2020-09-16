ad4dad60cefc25a335bb17636027a78c1194e6c4
Dima Murugov <dimamurugov@gmail.com>
2020-09-16 09:45:41 +0300

    fixed bag with change adres files data

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,13 @@
 const routercards = require('express').Router();
-const cards = require('../data/cards.json');
+const fs = require('fs').promises;
 
 routercards.get('/cards', (req, res) => {
-  res.send(cards);
+  fs.readFile('data/cards.json', 'utf-8')
+    .then((cards) => {
+      cards = JSON.parse(cards);
+      res.status(200).json(cards);
+    })
+    .catch(() => res.status(500).json({ message: 'Ошибка при чтении файла' }));
 });
 
 module.exports = { routercards };
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,32 @@
 const routerusers = require('express').Router();
-const users = require('../data/user.json');
-
-function findPerson(id) {
-  return users.find((item) => item._id === id);
-}
+const fs = require('fs').promises;
+const path = require('path');
 
 routerusers.get('/users', (req, res) => {
-  res.send(users);
+  fs.readFile('data/user.json', 'utf-8')
+    .then((users) => {
+      users = JSON.parse(users);
+      res.status(200).json(users);
+    })
+    .catch(() => res.status(500).json({ message: 'Ошибка при чтении файла' }));
 });
 
 routerusers.get('/users/:id', (req, res) => {
-  if (!findPerson(req.params.id)) {
-    res.send({ "message": "Нет пользователя с таким id" });
-  }
-  res.send(findPerson(req.params.id));
+  fs.readFile(path.join('data/user.json'), 'utf-8')
+    .then((users) => {
+      function findPerson(id) {
+        return users.find((item) => item._id === id);
+      }
+
+      users = JSON.parse(users);
+      if (!findPerson(req.params.id)) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+      res.status(200).send(findPerson(req.params.id));
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Ошибка при чтении файла' });
+    });
 });
 
 module.exports = { routerusers };
